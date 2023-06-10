d43ebcd4b69f013b1ffe47b3f308e2bdfb31368b
LucasDDiazCuenca <lucasDamian30@gmail.com>
2023-06-10 04:11:58 +0100

    add authenticateUser spec

--- staff/lucas-diaz/fullstack/api/logic/authenticateUser.spec.js
+++ staff/lucas-diaz/fullstack/api/logic/authenticateUser.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai")
-const { authenticateUser } = require("./authenticateUser")
+const authenticateUser = require("./authenticateUser")
 const { writeFile } = require("fs")
 
 describe("authenticateUser", () => {
@@ -28,7 +28,7 @@ describe("authenticateUser", () => {
 
         const json = JSON.stringify([user])
         //tenemos que escribir el archivo con un usuario 
-        
+
         writeFile("./data/users.json", json, "utf8", error => {
             expect(error).to.be.null
             //tenemos que comprobar que la funcion authenticateUser lo haga ok buscando en BBDD 
@@ -40,4 +40,94 @@ describe("authenticateUser", () => {
             })
         })
     })
+
+    it("should fail on authenticating user", done => {
+        const user = {
+            id,
+            name,
+            email,
+            password,
+            avatar,
+            savedPosts
+        }
+
+        authenticateUser(user.email, user.password, (error, userId) => {
+            expect(error).to.be.instanceOf(Error)
+            expect(error.message).to.equal(`there is no user with email: ${email}`)
+            expect(userId).to.be.undefined
+
+            done();
+        })
+    })
+
+    it("should fail on introducing wrong password", done => {
+        const user = {
+            id,
+            name,
+            email,
+            password,
+            avatar,
+            savedPosts
+        }
+
+        const json = JSON.stringify([user])
+        //tenemos que escribir el archivo con un usuario 
+
+        writeFile("./data/users.json", json, "utf8", error => {
+            expect(error).to.be.null
+            //tenemos que comprobar que la funcion authenticateUser lo haga ok buscando en BBDD 
+            authenticateUser(user.email, "helloFriends!22", (error, userId) => {
+                expect(error).to.be.instanceOf(Error)
+                expect(error.message).to.equal(`wrong credentials`)
+                done();
+            })
+        })
+    })
+
+    //sincronos de email
+    it("should fail on empty email", () => {
+        expect(() => authenticateUser( "" , "helloFriends!22", () => { })).to.throw(Error, "Email is empty")
+    })
+
+    it("should fail on non-string value in email", () => {
+        expect(() => authenticateUser(22, "helloFriends!22", () => { })).to.throw(Error, "Email is not a string")
+        expect(() => authenticateUser(true, "helloFriends!22", () => { })).to.throw(Error, "Email is not a string")
+        expect(() => authenticateUser({}, "helloFriends!22", () => { })).to.throw(Error, "Email is not a string")
+        expect(() => authenticateUser([], "helloFriends!22", () => { })).to.throw(Error, "Email is not a string")
+    })
+
+    it("should fail on invalid format in email", () => {
+        expect(() => authenticateUser("heloworld", "helloFriends!22", password, () => { })).to.throw(Error, 'Invalid email format')
+    })
+
+    it("should fail on blank space value in email", () => {
+        expect(() => authenticateUser(" ", "helloFriends!22", () => { })).to.throw(Error, "Email cant be a blankSpace")
+    })
+
+    // sincronos de pass 
+    it("should fail on empty password", () => {
+        expect(() => authenticateUser("hello@gmail.com", "", () => { })).to.throw(Error, "Password is empty")
+    })
+
+    it("should fail on non-string value in password", () => {
+        expect(() => authenticateUser("hello@gmail.com", 22, () => { })).to.throw(Error, "Password is not a string")
+        expect(() => authenticateUser("hello@gmail.com", true, () => { })).to.throw(Error, "Password is not a string")
+        expect(() => authenticateUser("hello@gmail.com", {}, () => { })).to.throw(Error, "Password is not a string")
+        expect(() => authenticateUser("hello@gmail.com", [], () => { })).to.throw(Error, "Password is not a string")
+    })
+
+    it("should fail on blank space value in password", () => {
+        expect(() => authenticateUser("hello@gmail.com", " ", () => { })).to.throw(Error, "Password cant be a blankSpace")
+    })
+
+    it("should fail on lower than 4 character password", () => {
+        expect(() => authenticateUser("hello@gmail.com", "bo", () => { })).to.throw(Error, "Password is shorter than 4 characters")
+    })
+
+    it("should fail on invalid format in password", () => {
+        expect(() => authenticateUser("hello@gmail.com", "helloworld", () => { })).to.throw(Error, `password format incorrect`)
+    })
+
+
+    after(done => writeFile("./data/users.json", "[]", "utf8", error => done(error)))
 })
