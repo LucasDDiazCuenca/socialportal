3394f133ab1bd6ac481abcb07996c5d613ab63ed
LucasDDiazCuenca <lucasDamian30@gmail.com>
2023-06-18 20:39:04 +0100

    renaming all mid functions in middlewares folder into Handler.js; renaming middleware folder into handlers; fix bugs in api's index.js

--- staff/lucas-diaz/fullstack/api/index.js
+++ staff/lucas-diaz/fullstack/api/index.js
@@ -1,43 +1,43 @@
 require("dotenv").config()
 const express = require("express")
 const { cors, jsonBodyParser } = require("./utils")
-const { helloApiMid, registerUserMid, authenticateUserMid, retrieveUserMid, updateUserAvatarMid, updateUserPasswordMid, createPostMid, retrievePostsMid, retrieveSavedPostsMid, retrieveUserPostsMid, retrievePostByPostIdMid, toggleHidePostMid, toggleLikePostMid, toggleSavePostInUserMid, updatePostMid, deletePostMid } = require("./middlewares")
+const { helloApiHandler, registerUserHandler, authenticateUserHandler, retrieveUserHandler, updateUserAvatarHandler, updateUserPasswordHandler, createPostHandler, retrievePostsHandler, retrieveSavedPostsHandler, retrieveUserPostsHandler, retrievePostByPostIdHandler, toggleHidePostHandler, toggleLikePostHandler, toggleSavePostInUserHandler, updatePostHandler, deletePostHandler } = require("./handlers")
 
 const api = express()
 api.use(cors)
-api.get("/", helloApiMid)
+api.get("/", helloApiHandler)
 
 //!USERS
-api.post("/users", jsonBodyParser, registerUserMid)
+api.post("/users", jsonBodyParser, registerUserHandler)
 
-api.post("/users/auth", jsonBodyParser, authenticateUserMid)
+api.post("/users/auth", jsonBodyParser, authenticateUserHandler)
 
-api.get("/users", retrieveUserMid)
+api.get("/users", retrieveUserHandler)
 
-api.patch("/users/avatar", jsonBodyParser, updateUserAvatarMid)
+api.patch("/users/avatar", jsonBodyParser, updateUserAvatarHandler)
 
-api.patch("/users/password", jsonBodyParser, updateUserPasswordMid)
+api.patch("/users/password", jsonBodyParser, updateUserPasswordHandler)
 
 //!POSTS
-api.post("/posts", jsonBodyParser, createPostMid)
+api.post("/posts", jsonBodyParser, createPostHandler)
 
-api.get("/posts", retrievePostsMid)
+api.get("/posts", retrievePostsHandler)
 
-api.get("/posts/saved", retrieveSavedPostsMid)
+api.get("/posts/saved", retrieveSavedPostsHandler)
 
-api.get("/posts/users", retrieveUserPostsMid)
+api.get("/posts/users", retrieveUserPostsHandler)
 
-api.get("/posts/:postId", retrievePostByPostIdMid)
+api.get("/posts/:postId", retrievePostByPostIdHandler)
 
-api.patch("/posts/hide/:postId", toggleHidePostMid)
+api.patch("/posts/hide/:postId", toggleHidePostHandler)
 
-api.patch("/posts/like/:postId", toggleLikePostMid)
+api.patch("/posts/like/:postId", toggleLikePostHandler)
 
-api.patch("/users/save/:postId", toggleSavePostInUserMid)
+api.patch("/users/save/:postId", toggleSavePostInUserHandler)
 
-api.patch("/posts/update/:postId", jsonBodyParser, updatePostMid)
+api.patch("/posts/update/:postId", jsonBodyParser, updatePostHandler)
 
-api.delete("/posts/delete/:postId", deletePostMid)
+api.delete("/posts/delete/:postId", deletePostHandler)
 
 api.listen(process.env.PORT, () => console.log(`Process running in port ${process.env.PORT}`))
 
--- staff/lucas-diaz/fullstack/api/utils/jsonBodyParser.js
+++ staff/lucas-diaz/fullstack/api/utils/jsonBodyParser.js
@@ -1,12 +1,10 @@
 module.exports = (req, res, next) => {
-    
     const { "content-type": contentType } = req.headers
 
     if (contentType !== "application/json"){
         res.status(400).json({error: "no application/json header"})
         return
     }
-
 
     let json = ""
 
@@ -21,6 +19,5 @@ module.exports = (req, res, next) => {
         }catch(error){
             res.status(400).json({error: error.message})
         }
-
     })
 }
