84247652e3ec6574c776b03a714dad40ac617842
LucasDDiazCuenca <lucasDamian30@gmail.com>
2023-07-15 03:01:55 +0100

    implement isUserLoggedIn logic in App.jsx in order to refactor code

--- staff/lucas-diaz/fullstack/app-React/src/App.jsx
+++ staff/lucas-diaz/fullstack/app-React/src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import context from "./logic/context";
 import Alert from "./components/Alert";
 import AppContext from "./AppContext";
 import CreateCube from "./components/CreateCube";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom"
-import { utils } from "com"
+import isUserLoggedIn from "./logic/isUserLoggedIn.js"
 
 const { Provider } = AppContext
-const { isTokenAlive, isTokenValid } = utils
+
 
 export default function App() {
   const [feedback, setFeedback] = useState(null)
@@ -24,9 +23,9 @@ export default function App() {
 
   return <Provider value={{ Alert: setFeedback, freeze, unFreeze, navigate }}>
     <Routes>
-      <Route path="/login" element={isTokenValid(context.token) && isTokenAlive(context.token) ? <Navigate to="/" /> : <Login />} />
-      <Route path="/register" element={isTokenValid(context.token) && isTokenAlive(context.token) ? <Navigate to="/" /> : <Register />} />
-      <Route path="/" element={isTokenValid(context.token) && isTokenAlive(context.token) ? <Home /> : <Navigate to="/login" />} />
+      <Route path="/login" element={isUserLoggedIn() ? <Navigate to="/" /> : <Login />} />
+      <Route path="/register" element={isUserLoggedIn() ? <Navigate to="/" /> : <Register />} />
+      <Route path="/" element={isUserLoggedIn() ? <Home /> : <Navigate to="/login" />} />
     </Routes>
 
     {feedback && <Alert message={feedback} onAccept={handleAcceptAlert} />}
